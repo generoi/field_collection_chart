251793d7475f107d0b21b568042aa071a5b56ffa
Oskar Schöldström <public@oxy.fi>
2015-07-23 08:17:34 -0500

    fix charts not initialized while within foundation sections

--- js/graphael_chart.js
+++ js/graphael_chart.js
@@ -3,11 +3,28 @@
     attach: function(context) {
       var settings = Drupal.settings.graphaelChart || {};
       if (settings.charts) {
+        var delayed = [];
         for (var chart in settings.charts) if (settings.charts.hasOwnProperty(chart)) {
           var options = settings.charts[chart];
           // Only initialize a chart once.
           if (!options.type) continue;
+
+          if ($('#' + chart).not(':visible')) {
+            delayed.push(chart);
+            continue;
+          }
           Drupal.graphaelChart[options.type](chart, options);
+        }
+        if (delayed.length) {
+          $(window).one('resize.fndtn.section', function() {
+            for (var i = 0, l = delayed.length; i < l; i++) {
+              var chart = delayed[i]
+                , options = settings.charts[chart];
+
+              if (!options.type) continue;
+              Drupal.graphaelChart[options.type](chart, options);
+            }
+          });
         }
       }
     }
